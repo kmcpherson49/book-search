3e7675d16ded5f1f35cb5c1f416d63f87fb72bc1
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-07-05 13:53:51 -0700

    add typedefs and reseolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors(authors: String): [authors]
+    description: String
+    title: String
+    image: image
+    link: link
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook([Author]): User
+    removeBook(id: bookID): User
+  }
+`;
+
+module.exports = typeDefs;
